5a2b19380397c23f8fe9989559b8756992592be6
Tassio Med <tassiohfmed@gmail.com>
2022-09-06 15:12:27 -0300

    :tada: Initial commit

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const cors = require('cors');
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
-const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
+const StrangerThingsRepository = require('./data/repository/StrangerThings');
 
 const app = express();
 
